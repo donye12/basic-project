afd1eacb8d68274ec0943ffde6c3f153be9c6c61
donye12 <donye.bidds@gmail.com>
2024-05-03 23:39:14 -0400

    adds junit tests

--- src/test/java/com/example/test/demo/DemoApplicationTests.java
+++ src/test/java/com/example/test/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.test.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void controllerTest() {
+		String response = testRestTemplate.getForObject("http://localhost:" + port + "/", String.class);
+
+		assertEquals("here", response);
 	}
 
 }
